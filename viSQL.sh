adc1c84acb5b883a08445c4d8907a5c79989baac
tchelospy <tchelospy@gmail.com>
2017-08-01 13:25:41 -0300

    Update viSQL.sh

--- viSQL.sh
+++ viSQL.sh
@@ -2,6 +2,8 @@ echo "##########################################"
 echo "		    PHONE-HACKER		"
 echo "##########################################"
 
+sudo su
+
 apt update && upgrade
 apt install git
 git clone https://github.com/blackvkng/viSQL.git
@@ -9,4 +11,5 @@ git clone https://github.com/blackvkng/viSQL.git
 cd viSQL
 
 python2 -m pip install -r requirements.txt
+
 python2 viSQL.py --help
